fd79a528e510d431ec5b85b65dbfe79daa704825
br1lisboa <brlisbo@gmail.com>
2022-11-16 11:54:14 -0300

    image ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="Container">
       <Login />
-      <ProductsList />
       <SlideBar />
+      <ProductsList />
     </div>
   );
 }
--- frontend/src/views/SlideBar.jsx
+++ frontend/src/views/SlideBar.jsx
@@ -3,11 +3,11 @@ export const SlideBar = () => {
     return (
         <div className="flex bg-red-100">
 
-            <span className="flex-1 border-2 flex flex-col justify-center items-center h-96 text-9xl">
+            <span className="flex-1 flex flex-col justify-center items-center h-96 text-9xl">
                 50%
             </span>
 
-            <div className="flex-1 border-2">
+            <div className="flex-1">
 
                 <div className="flex flex-col justify-center items-center h-96">
 
@@ -21,8 +21,8 @@ export const SlideBar = () => {
 
             </div>
 
-            <div className="flex-1 border-2">
-                <img className="h-full" src="https://caminandog.com.mx/b/wp-content/uploads/2020/08/hipo-perritos-Blog04.jpg" alt="" />
+            <div className="flex-1">
+                <img className="h-full" src="../src/assets/perritoSlideBar-sinBG.png" alt="" />
             </div>
 
         </div>
