6b8df0f4514d5d20338c95b026970f703aa73ecb
Andrey Gomez <afgomezvonline@gmail.com>
2022-11-16 00:09:07 -0500

    primer commit frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Login from "./views/Login";
+import ProductsList from "./views/ProductsLists";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="Container">
       <Login />
+      <ProductsList />
     </div>
   );
 }
