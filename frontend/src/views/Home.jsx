4e542b4ea62901e8ac8c8bd5b1df154d77ed1c55
alexisarte <alexisrodriguezarteaga@gmail.com>
2022-11-18 18:50:59 -0300

    Add the services section to the home page

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { SlideBar } from './SlideBar';
 import NavBar from './NavBar';
+import Services from './Services';
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       </button>
       <NavBar />
       <SlideBar />
+      <Services />
     </>
   );
 };
