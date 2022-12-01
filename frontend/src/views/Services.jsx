a4ae415a223d5849d62992b336540a439fd156e2
alexis <alexisrodriguezarteaga@gmail.com>
2022-12-01 13:02:10 -0300

    Make the services section Responsive

--- frontend/src/views/Services.jsx
+++ frontend/src/views/Services.jsx
@@ -1,14 +1,21 @@
 const Services = () => {
   return (
-    <section className="md:text-6xl font-new">
-      <p className="uppercase text-blue-600 p-4">en <span className="text-yellow-400">kai pet</span> te contactamos con los mejores centros veterinarios, cercanos a tu domicilio.</p>
-      <div className="flex h-96  bg-blue-400">
-        <ul className="flex flex-col justify-between w-3/4 list-inside list-disc uppercase p-6">
+    <section className="text-5xl font-fontFresca font-new flex flex-wrap flex-col-reverse md:flex-row">
+      <p className="uppercase text-blue-600 p-4">
+        en <span className="text-yellow-400">kai pet</span> te contactamos con
+        los mejores centros veterinarios, cercanos a tu domicilio.
+      </p>
+      <div className="flex flex-col-reverse md:flex-row max-xl:flex-wrap max-xl:justify-center bg-blue-400">
+        <ul className="flex flex-col justify-around items-center px-8 list-inside list-disc uppercase p-6">
           <li>consulta de veterinaria</li>
           <li>procedimientos quirúrjicos</li>
           <li>urgencias y asistencia médica</li>
         </ul>
-        <img className="w-1/4" src="../src/assets/veterinaria.png" alt="veterinaria" />
+        <img
+          className=""
+          src="../src/assets/veterinaria.png"
+          alt="veterinaria"
+        />
       </div>
     </section>
   );
